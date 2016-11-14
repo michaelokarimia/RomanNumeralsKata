6642b9f76aa6eef202083a3250d6ac4b524feba3
Michael Okarimia <git@michaelokarimia.com>
2016-11-14 22:04:49 +0000

    Inital commit

---
+++ RomanNumerals/NumeralPrinter.cs
@@ -0,0 +1,10 @@
+﻿namespace RomanNumerals
+{
+    public class NumeralPrinter
+    {
+        public static string Print(int numeral)
+        {
+            return "i";
+        }
+    }
+}
---
+++ RomanNumeralsTest/NumeralPrinterTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using RomanNumerals;
+
+namespace RomanNumeralsTest
+{
+    [TestFixture]
+    public class NumeralPrinterTests
+    {
+
+        [TestCase(1,"i")]
+        [Test]
+        public void Converts_1_into_i(int arabicNumeral, string expectedNumeral)
+        {
+            Assert.That(NumeralPrinter.Print(arabicNumeral), Is.EqualTo(expectedNumeral));
+        }
+    }
+}
